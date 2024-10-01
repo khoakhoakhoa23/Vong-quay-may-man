8928077881920c34827890101d17813395fca50d
khoakhoakhoa23 <tanhkhoa06@gmail.com>
2024-10-01 15:43:16 +0700

    khoi dau

---
+++ hello.cs
@@ -0,0 +1,9 @@
+using System;
+namespace nhocac{
+    internal class Program{
+        static void Main([string[]  args]){
+            Console.WriteLine("nhocngu");
+            Console.ReadKey();
+        }
+    }
+}
