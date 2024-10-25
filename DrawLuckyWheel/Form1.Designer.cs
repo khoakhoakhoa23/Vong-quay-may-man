51017e0ef4742874de6db2985573cf9a06d5f0e2
khoakhoakhoa23 <2251120095@ut.edu.vn>
2024-10-25 09:42:48 +0700

    demo01

--- DrawLuckyWheel/Form1.Designer.cs
+++ DrawLuckyWheel/Form1.Designer.cs
@@ -37,9 +37,19 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // btnPlay
@@ -116,7 +126,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::DrawLuckyWheel.Properties.Resources.placeholder;
-            this.pictureBox2.Location = new System.Drawing.Point(365, 128);
+            this.pictureBox2.Location = new System.Drawing.Point(359, 128);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(76, 64);
@@ -124,25 +134,118 @@
             this.pictureBox2.TabIndex = 12;
             this.pictureBox2.TabStop = false;
             // 
-            // pictureBox1
+            // pictureBox
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(121, 200);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(554, 546);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(121, 200);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(556, 545);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.textBox2.Location = new System.Drawing.Point(505, 365);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(29, 22);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Red;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(428, 285);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(25, 22);
+            this.textBox1.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox3.Location = new System.Drawing.Point(545, 459);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(29, 22);
+            this.textBox3.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox4.Location = new System.Drawing.Point(521, 561);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(30, 22);
+            this.textBox4.TabIndex = 21;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.Yellow;
+            this.textBox5.Location = new System.Drawing.Point(439, 604);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(25, 22);
+            this.textBox5.TabIndex = 22;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.Red;
+            this.textBox6.Location = new System.Drawing.Point(334, 617);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(28, 22);
+            this.textBox6.TabIndex = 23;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.textBox7.Location = new System.Drawing.Point(225, 561);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(30, 22);
+            this.textBox7.TabIndex = 24;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox8.Location = new System.Drawing.Point(185, 459);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(31, 22);
+            this.textBox8.TabIndex = 25;
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox9.Location = new System.Drawing.Point(239, 355);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(29, 22);
+            this.textBox9.TabIndex = 26;
+            // 
+            // textBox10
+            // 
+            this.textBox10.BackColor = System.Drawing.Color.Yellow;
+            this.textBox10.Location = new System.Drawing.Point(325, 285);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(37, 22);
+            this.textBox10.TabIndex = 27;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1163, 820);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
@@ -151,12 +254,12 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnPlay);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "[C#] Lucky Game - https://laptrinhvb.net";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,7 +267,7 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.Button btnPlay;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox2;
@@ -173,6 +276,16 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
     }
 }
 
--- DrawLuckyWheel/Form1.cs
+++ DrawLuckyWheel/Form1.cs
@@ -12,7 +12,7 @@ namespace DrawLuckyWheel
         Timer wheelTimer;
         LuckyCirlce koloFortuny;
         List<int> inputNumbers;  // List to store the numbers entered by the user
-        List<Label> wheelLabels;  // List to hold the labels for each segment
+        List<TextBox> wheelTextBoxes;  // List to hold the pre-existing TextBoxes
 
         public Form1()
         {
@@ -21,7 +21,13 @@ namespace DrawLuckyWheel
             wheelTimer.Interval = 30; // Speed 
             wheelTimer.Tick += wheelTimer_Tick;
             inputNumbers = new List<int>();  // Initialize the list to store numbers
-            wheelLabels = new List<Label>();  // Initialize the list to store labels
+
+            // Instead of creating new TextBoxes, add existing TextBoxes from the form
+            wheelTextBoxes = new List<TextBox>
+            {
+                textBox1, textBox2, textBox3, textBox4, textBox5,
+                textBox6, textBox7, textBox8, textBox9, textBox10 // Add all pre-existing TextBoxes here
+            };
         }
 
         public class LuckyCirlce
@@ -31,7 +37,6 @@ namespace DrawLuckyWheel
             public int[] wartosciStanu;
             public int stan;
 
-            // Modify constructor to accept numbers
             public LuckyCirlce(int[] numbers)
             {
                 obrazek = new Bitmap(Properties.Resources.lucky_wheel);  // Replace with your image resource
@@ -52,73 +57,39 @@ namespace DrawLuckyWheel
 
             Bitmap rotatedBmp = new Bitmap(image.Width, image.Height);
             rotatedBmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-            Graphics g = Graphics.FromImage(rotatedBmp);
-            g.TranslateTransform(offset.X, offset.Y);
-            g.RotateTransform(angle);
-            g.TranslateTransform(-offset.X, -offset.Y);
-            g.DrawImage(image, new PointF(0, 0));
+            using (Graphics g = Graphics.FromImage(rotatedBmp))
+            {
+                g.TranslateTransform(offset.X, offset.Y);
+                g.RotateTransform(angle);
+                g.TranslateTransform(-offset.X, -offset.Y);
+                g.DrawImage(image, new PointF(0, 0));
+            }
 
             return rotatedBmp;
         }
 
-        // Create the labels for each sector and position them inside the wheel's segments
-        private void CreateWheelLabels(int[] numbers)
-        {
-            float sectorAngle = 360f / numbers.Length; // Angle per sector
-            PointF center = new PointF(200,200); // Center of the wheel
-            float radius = pictureBox1.Width / 2 - 60;  // Adjust based on your wheel size
-
-            // Clear any existing labels
-            foreach (var label in wheelLabels)
-            {
-                this.Controls.Remove(label);
-            }
-            wheelLabels.Clear();
-
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                Label label = new Label();
-                label.Text = numbers[i].ToString();
-                label.AutoSize = true;
-                label.BackColor = Color.Transparent;
-                label.Font = new Font("Arial", 16, FontStyle.Bold);
-                label.ForeColor = Color.Black;
-
-                // Add the label to the form and to the list
-                this.Controls.Add(label);
-                wheelLabels.Add(label);
-            }
-
-            // Initially position the labels based on their sector
-            UpdateLabelPositions(numbers.Length, 0);
-        }
-
-        // Update the positions of the labels as the wheel rotates
-        // Update the positions of the labels as the wheel rotates
         private void UpdateLabelPositions(int numSegments, float angleOffset)
         {
             float sectorAngle = 360f / numSegments; // Angle per sector
-            PointF center = new PointF(pictureBox1.Width / 2, pictureBox1.Height / 2); // Center of the wheel
-            float radius = pictureBox1.Width / 2 - 100;  // Adjust this to fit the labels within the wheel
+            PointF center = new PointF(pictureBox.Width / 2, pictureBox.Height / 2); // Center of the wheel
+            float radius = pictureBox.Width / 2 - 50;  // Adjust this to fit the textboxes within the wheel
 
             for (int i = 0; i < numSegments; i++)
             {
-                // Calculate the angle for each label considering the wheel's current rotation
                 float angle = (i * sectorAngle + angleOffset - 90) * (float)Math.PI / 180;
 
-                // Calculate the new position based on the angle and radius from the center
-                float x = center.X + (float)(radius * Math.Cos(angle)) - wheelLabels[i].Width / 2;
-                float y = center.Y + (float)(radius * Math.Sin(angle)) - wheelLabels[i].Height / 2;
+                float x = center.X + (float)(radius * Math.Cos(angle)) - wheelTextBoxes[i].Width / 2;
+                float y = center.Y + (float)(radius * Math.Sin(angle)) - wheelTextBoxes[i].Height / 2;
 
-                // Apply the calculated position to the label
-                wheelLabels[i].Location = new Point((int)x, (int)y);
+                wheelTextBoxes[i].Location = new Point((int)x, (int)y);
 
-                // Optionally rotate the label text to stay upright or match the segment's angle
-                wheelLabels[i].Text = inputNumbers[i].ToString();  // Update text if needed
+                if (i < inputNumbers.Count)
+                {
+                    wheelTextBoxes[i].Text = inputNumbers[i].ToString();
+                }
             }
         }
 
-        // Handle the timer tick to rotate both the wheel and the labels
         private void wheelTimer_Tick(object sender, EventArgs e)
         {
             if (wheelIsMoved && wheelTimes > 0)
@@ -126,33 +97,17 @@ namespace DrawLuckyWheel
                 koloFortuny.kat += wheelTimes / 10;
                 koloFortuny.kat = koloFortuny.kat % 360;
 
-                // Rotate the wheel image
-                pictureBox1.Image = RotateImage(koloFortuny.obrazek, koloFortuny.kat);
-
-                // Update the label positions to rotate with the wheel
+                pictureBox.Image = RotateImage(koloFortuny.obrazek, koloFortuny.kat);
                 UpdateLabelPositions(koloFortuny.wartosciStanu.Length, koloFortuny.kat);
 
                 wheelTimes--;
             }
 
-            // Adjusted calculation to prevent out-of-range errors
-            koloFortuny.stan = Convert.ToInt32(Math.Ceiling(koloFortuny.kat / 30)) - 1;
+            koloFortuny.stan = Convert.ToInt32(Math.Ceiling(koloFortuny.kat / (360f / koloFortuny.wartosciStanu.Length))) % koloFortuny.wartosciStanu.Length;
 
-            // Ensure stan is within bounds
-            if (koloFortuny.stan < 0)
-            {
-                koloFortuny.stan = 0;
-            }
-            else if (koloFortuny.stan >= koloFortuny.wartosciStanu.Length)
-            {
-                koloFortuny.stan = koloFortuny.wartosciStanu.Length - 1;
-            }
-
-            // Update the label text (if you want to show the current selected value)
             label1.Text = Convert.ToString(koloFortuny.wartosciStanu[koloFortuny.stan]);
         }
 
-        // Start the wheel spinning
         private void btnPlay_Click(object sender, EventArgs e)
         {
             if (inputNumbers.Count == 0)
@@ -161,11 +116,8 @@ namespace DrawLuckyWheel
                 return;
             }
 
-            // Initialize the lucky wheel with the entered numbers
             koloFortuny = new LuckyCirlce(inputNumbers.ToArray());
-
-            // Create the labels for the wheel (they will rotate with the wheel)
-            CreateWheelLabels(inputNumbers.ToArray());
+            UpdateLabelPositions(inputNumbers.Count, 0);
 
             wheelIsMoved = true;
             Random rand = new Random();
@@ -174,13 +126,13 @@ namespace DrawLuckyWheel
             wheelTimer.Start();
         }
 
-        // Add a number to the wheel
         private void btnAddNumber_Click(object sender, EventArgs e)
         {
             if (int.TryParse(textBoxNumber.Text, out int number))
             {
                 inputNumbers.Add(number);
                 textBoxNumber.Clear();  // Clear the input box for the next number
+                UpdateLabelPositions(inputNumbers.Count, 0);
             }
             else
             {
